2d69905a9e72576ad121b17859912f9fa943da44
brian <brian@unknown0026B9F59F06.attlocal.net>
2017-08-17 12:21:38 -0500

    foobar

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-JENKINS_WAR=/usr/local/jenkins/jenkins.war
+JENKINS_WAR=/opt/jenkins/jenkins.war
+JENKINS_OPTS=" --httpPort=8000 "
 JENKINS_LOG=/home/jenkins/jenkins.log
-JAVA=/usr/local/java/bin/java
-nohup nice $JAVA -jar $JENKINS_WAR > $JENKINS_LOG 2>&1 &
-Raw
-
+JAVA=/usr/bin/java
+nohup nice $JAVA -jar $JENKINS_WAR $JENKINS_OPTS > $JENKINS_LOG 2>&1 &
